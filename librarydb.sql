a94e18daf7ce52c8d63d19da287a4991c16a2406
DanieleNegro <danieleweb1987@gmail.com>
2024-10-09 14:22:09 +0200

    Change schema only math books

--- librarydb.sql
+++ librarydb.sql
@@ -1,3 +1,5 @@
+DROP DATABASE librarydb;
+
 CREATE DATABASE librarydb;
 
 USE librarydb;
@@ -7,37 +9,54 @@ CREATE TABLE
         id int NOT NULL AUTO_INCREMENT,
         title VARCHAR(255),
         author VARCHAR(255),
-        available CHAR(),
-        category VARCHAR(255),
+        available CHAR(1),
+        category INT,
         PRIMARY KEY (id)
     );
 
 INSERT INTO
     books (title, author, available, category)
 VALUES
-    ('Ulysses', 'James Joyce', 'Y', 'Fiction');
-
+    ('Algebra II All-in-One For Dummies', 'Mary Jane Sterling', 'Y', 0);
 INSERT INTO
     books (title, author, available, category)
 VALUES
     (
-        'A Farewell to Arms',
-        'Ernest Hemingway',
+        'Calculus II Workbook For Dummies',
+        'Mark Zegarelli',
         'N',
-        'Fiction'
+        1
     );
-
 INSERT INTO
     books (title, author, available, category)
 VALUES
     (
-        'I Know Why the Caged Bird Sings',
-        'Maya Angelou',
+        'Calculus All-in-One For Dummies',
+        ' Mark Ryan',
         'Y',
-        'Poetry'
+        1
     );
-
 INSERT INTO
     books (title, author, available, category)
 VALUES
-    ('Moby Dick', 'Herman Melville', 'Y', 'Fiction');
+    ('Statistics All-in-One For Dummies', 'Deborah J. Rumsey', 'Y', 3);
+INSERT INTO
+    books (title, author, available, category)
+VALUES
+    ('Geometry: 1001 Practice Problems For Dummies', 'Allen Ma , Amber Kuang', 'Y', 2);
+INSERT INTO
+    books (title, author, available, category)
+VALUES
+    ('Algebra I All-in-One For Dummies', 'Mary Jane Sterling', 'N', 0);
+INSERT INTO
+    books (title, author, available, category)
+VALUES
+    ('Statistics II For Dummies', 'Deborah J. Rumsey', 'Y', 3);
+INSERT INTO
+    books (title, author, available, category)
+VALUES
+    ('Trigonometry For Dummies', 'Mary Jane Sterling', 'Y', 4);
+INSERT INTO
+    books (title, author, available, category)
+VALUES
+    ('Trigonometry Workbook For Dummies', 'Mary Jane Sterling', 'N', 4);
